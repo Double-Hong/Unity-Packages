09f7dc3cd033df29fa5d1f7fce8119504366f30a
DoubleHong <greathongtian@gmail.com>
2025-04-06 00:04:38 +0800

    Myh Packages

---
+++ com.myh.screenfitter/Runtime/ScreenFitter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace myh
+{
+    public class ScreenFitter : MonoBehaviour
+    {
+        private void Start()
+        {
+            FitScreen();
+        }
+
+        /// <summary>
+        /// 屏幕适配
+        /// </summary>
+        private void FitScreen()
+        {
+            CanvasScaler cs = transform.root.GetComponentInChildren<CanvasScaler>();
+#if UNITY_EDITOR
+            Rect safeArea = Screen.safeArea;
+            float py = (float)safeArea.yMin / (float)safeArea.height;
+            transform.GetComponent<RectTransform>().anchorMin = new Vector2(
+                (float)safeArea.left / (float)safeArea.width, -(float)safeArea.top / (float)safeArea.height);
+            cs.referenceResolution = new Vector2(cs.referenceResolution.x, cs.referenceResolution.y * (1.0f + py));
+
+#elif minigame
+            var info = WX.GetWindowInfo();
+            float py = (float)info.safeArea.top / (float)info.windowHeight;
+            transform.GetComponent<RectTransform>().anchorMin =
+                new Vector2((float)info.safeArea.left / (float)info.windowWidth,
+                    -(float)info.safeArea.top / (float)info.windowHeight);
+            cs.referenceResolution = new Vector2(cs.referenceResolution.x, cs.referenceResolution.y * (1.0f + py));
+#endif
+        }
+    }
+}
